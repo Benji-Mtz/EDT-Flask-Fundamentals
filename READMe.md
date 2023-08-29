bff2565ffc14b9140fe4c8d28247df94944d5226
Benji Martinez <benjim.flores@outlook.com>
2023-08-29 17:46:24 -0600

    first init

---
+++ READMe.md
@@ -0,0 +1,12 @@
+# Run project with
+
+```sh
+python3 -m venv .venv
+source .venv/bin/activate
+.venv/Scripts/activate.bat
+pip install flask
+pip
+pip install -r requirements.txt
+```
+
+For `.env` files we will need to install `pip install python-dotenv`
---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return """
+    <h1 style="font-size: 64px; color:blue;"> Hola Mundo!</h1>
+    """
+
+
+@app.route('/contacto')
+def contact():
+    return """
+    <h1 style="font-size: 64px; color:blue;"> Contact</h1>
+    """
+
+
+@app.route('/nosotros')
+def our():
+    return """
+    <h1 style="font-size: 64px; color:blue;"> Nosotros</h1>
+    """
+
+
+@app.route('/login/<name>')
+@app.route('/login/')
+def login(name=None):
+    if name is not None:
+        return redirect(f'/dashboard/{ name }')
+    else:
+        return f'<h1 style="font-size: 32px; color:blue;">Ingresa tu nombre en la Url...</h1>'
+
+
+@app.route('/dashboard/<name>')
+@app.route('/dashboard/')
+def dashboard(name=None):
+    if name is not None:
+        return f'<h1 style="font-size: 16px; color:green;">Bienvenido (a) { name } al dashboard</h1>'
+    return redirect("/login/")
+
+
+"""
+@app.route('/saludo/<name>/<int:sueldo>')
+@app.route('/saludo/<name>')
+@app.route('/saludo/')
+def greeting(name=None, sueldo=None):
+    if name is not None:
+        if sueldo is not None:
+            return f'<h1 style="font-size: 64px; color:blue;">Hola: { name }, tu sueldo es: { sueldo * 2 }</h1>'
+        return f'<h1 style="font-size: 64px; color:blue;">Hola: { name }</h1>'
+    else:
+        return f'<h1 style="font-size: 64px; color:blue;">Hola desconocido</h1>'
+"""
